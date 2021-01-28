9859606812c934d7f958a3cd472e3ab2f9172cc3
BERNA ÖZGEN <brnozgen@hotmail.com>
2021-01-29 01:45:55 +0300

    ders4

--- Koleksiyonlar/Program.cs
+++ Koleksiyonlar/Program.cs
@@ -7,7 +7,16 @@ namespace Koleksiyonlar
     {
         static void Main(string[] args)
         {
-            List<string> isimler2 = new List<string>();
+            List<string> isimler2 = new List<string>{"Engin","Murat","Kerem","Halil"};
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("BERNA");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+
+
         }
     }
 }
