d5f098440320185389016fd68c03335029a4de0b
BERNA ÖZGEN <brnozgen@hotmail.com>
2021-02-21 20:23:12 +0300

    Interface baslangıc

---
+++ OOP2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri gercekMusteri1 = new GercekMusteri();
+            gercekMusteri1.Id = 1;
+            gercekMusteri1.MusteriNo = "123";
+            gercekMusteri1.Adi = "Berna";
+            gercekMusteri1.Soyadi = "Özgen";
+            gercekMusteri1.TcNo = "12345678978";
+
+
+
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "124";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "123456789";
+
+
+
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+            musteri3.Id = 12;
+            musteri3.MusteriNo = "1234";
+
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(gercekMusteri1);
+            musteriManager.Add(musteri2);
+            musteriManager.Add(musteri3);
+            musteriManager.Add(musteri4);
+            
+
+        }
+    }
+}
---
+++ OOP2/TuzelMusteri.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    
+    //Coorporate
+    class TuzelMusteri:Musteri//bu olaya miras yada inheritance denir 
+    {
+       
+        public string SirketAdi { get; set; }
+        public string VergiNo  { get; set; }
+
+    }
+}
