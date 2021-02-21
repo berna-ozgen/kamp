0cd2b9d031c5e2a1436f7a29ddb0015c7dbc1a5f
BERNA ÖZGEN <brnozgen@hotmail.com>
2021-02-21 20:22:29 +0300

    Banka simülasyonu

---
+++ OOP3/BasvuruManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace OOP3
+{
+    class BasvuruManager
+    {
+        // Method injection
+        public void BasvuruYap(IKrediManager krediManager, List<ILoggerService> loggerServices)
+        {
+            //basvuran bilgilerini degerlendirme yaparz 
+            krediManager.Hesapla();//Kredi bagımsız olarak hesaplama yapıyoruz.
+            foreach (var loggerService in loggerServices)
+            {
+                loggerService.Log();
+
+            }
+        }
+        public void KrediOnBilgilendirmesiYap(List<IKrediManager> krediler)
+        {
+            foreach (var kredi in krediler)
+            {
+                kredi.Hesapla();
+            }
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ekranlar acılır pencere gibi dusun 
+            IKrediManager ıhtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            IKrediManager esnafKrediManager = new EsnafKrediManager();
+            //ekranda cagırma yapacagımız yerler 
+            BasvuruManager basvuruManager = new BasvuruManager();
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService= new FileLoggerService();
+            ILoggerService smsLoggerService = new SmsLoggerService();
+
+           // basvuruManager.BasvuruYap(tasitKrediManager, databaseLoggerServic);
+            //basvuruManager.BasvuruYap(ıhtiyacKrediManager,fileLoggerService);
+
+            List<ILoggerService> logger = new List<ILoggerService>() { smsLoggerService, databaseLoggerService }; //ekranda secilen logları aldı varsayılıyor.        
+            basvuruManager.BasvuruYap(esnafKrediManager, logger);
+            List<ILoggerService> loggers = new List<ILoggerService>() { smsLoggerService, fileLoggerService };
+            basvuruManager.BasvuruYap(konutKrediManager,loggers);
+
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ıhtiyacKrediManager,tasitKrediManager};//ekrandan secilenleri liste tutup birden fazla hesaplama yaparız
+           //basvuruManager.KrediOnBilgilendirmesiYap(krediler); 
+        }
+    }
+}
